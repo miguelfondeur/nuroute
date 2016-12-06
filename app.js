71a285d6d6d5e08252ad3d93242c7e25375f2f20
miguel fondeur <mfondeur@theladders.com>
2016-12-06 17:07:15 -0500

    update app structure

--- app.js
+++ app.js
@@ -1,47 +1,61 @@
 // change this file
+'use strict';
 
-require('babel-register')
+const express = require('express');
+const port = 3000;
 
-const express = require('express')
-const React = require('react')
-const ReactDOMServer = require('react-dom/server')
-const ReactRouter = require('react-router')
-const match = ReactRouter.match
-const RouterContext = ReactRouter.RouterContext
-const ReactRedux = require('react-redux')
-const Provider = ReactRedux.Provider
-const Store = require('./js/Store.jsx')
-const store = Store.store
-const _ = require('lodash')
-const fs = require('fs')
-const port = 5050
-const baseTemplate = fs.readFileSync('./index.html')
-const template = _.template(baseTemplate)
-const ClientApp = require('./js/ClientApp.jsx')
-const routes = ClientApp.Routes
+const app = express();
 
-const app = express()
+app.get('/', function(req, res){
+  res.send("<h1>WOW!</h1>")
+});
 
-app.use('/public', express.static('./public'))
+app.listen(port, function(){
+  console.log("The frontend server is running on port 3000");
+});
 
-app.use((req, res) => {
-  match({ routes: routes, location: req.url }, (error, redirectLocation, renderProps) => {
-    if (error) {
-      res.status(500).send(error.message)
-    } else if (redirectLocation) {
-      res.redirect(302, redirectLocation.pathname + redirectLocation.search)
-    } else if (renderProps) {
-      const body = ReactDOMServer.renderToString(
-        React.createElement(Provider, {store},
-          React.createElement(RouterContext, renderProps)
-        )
-      )
-      res.status(200).send(template({body}))
-    } else {
-      res.status(404).send('Not found')
-    }
-  })
-})
+// require('babel-register')
 
-console.log('Express server listening on port ' + port)
-app.listen(port)
+// const express = require('express')
+// const React = require('react')
+// const ReactDOMServer = require('react-dom/server')
+// const ReactRouter = require('react-router')
+// const match = ReactRouter.match
+// const RouterContext = ReactRouter.RouterContext
+// const ReactRedux = require('react-redux')
+// const Provider = ReactRedux.Provider
+// const Store = require('./js/Store.jsx')
+// const store = Store.store
+// const _ = require('lodash')
+// const fs = require('fs')
+// const baseTemplate = fs.readFileSync('./index.html')
+// const template = _.template(baseTemplate)
+// const ClientApp = require('./js/ClientApp.jsx')
+// const routes = ClientApp.Routes
+
+// const app = express()
+
+// app.use('/public', express.static('./public'))
+
+// app.use((req, res) => {
+//   match({ routes: routes, location: req.url }, (error, redirectLocation, renderProps) => {
+//     if (error) {
+//       res.status(500).send(error.message)
+//     } else if (redirectLocation) {
+//       res.redirect(302, redirectLocation.pathname + redirectLocation.search)
+//     } else if (renderProps) {
+//       const body = ReactDOMServer.renderToString(
+//         React.createElement(Provider, {store},
+//           React.createElement(RouterContext, renderProps)
+//         )
+//       )
+//       res.status(200).send(template({body}))
+//     } else {
+//       res.status(404).send('Not found')
+//     }
+//   })
+// })
+
+// console.log('Express server listening on port ' + port)
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,26 @@
 
 module.exports = {
   context: __dirname,
-  entry: './static/src/index.jsx',
-
+  entry: './static/src/scripts/index.jsx',
   output: {
-    path: path.join(__dirname, './static/build/js'),
+    path: path.join(__dirname, './static/public/js'),
     filename: 'bundle.js'
   },
    resolve: {
     extensions: ['', '.js', '.jsx', '.json']
   },
-
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: false
+  },
   module: {
     loaders: [
       {
         test: /\.js$/, 
         exclude: /node_modules/, 
         loaders: ['react-hot', 'babel-loader']
-      },
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader'
       }
     ]
   }
-
 };
