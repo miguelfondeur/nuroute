0d22f91e70b9ac5b478555a3c7e3a249e434dd1d
Miguel Fondeur <miguelfondeur@gmail.com>
2016-12-30 20:02:11 -0500

    add new jsx files

---
+++ static/src/scripts/components/avatar/Avatar.jsx
@@ -0,0 +1,14 @@
+// Libs
+import React from 'react';
+
+// CSS
+import styles from './Avatar.css';
+
+const Avatar = () => (
+	<div className={styles.avatar}>
+		<img src="" />
+	</div>
+);
+
+// Export
+module.exports = Avatar;
---
+++ static/src/scripts/components/button/Button.jsx
@@ -0,0 +1,12 @@
+// Libs
+import React from 'react';
+
+// CSS
+// import styles from './Button.css';
+
+const Button = () => (
+	<button />
+);
+
+// Export
+module.exports = Button;
